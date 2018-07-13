d4c21fb83c3b66b88be116bef873a51fa3ac1ebb
David Raditya K <32066453+davidraditya@users.noreply.github.com>
2018-07-13 10:49:36 +0800

    v7.13.18

--- prep_env.sh
+++ prep_env.sh
@@ -4,5 +4,7 @@ cd
 echo Update all apps...
 sudo apt-get update
 
-echo Change hostname to "oai-pc"...
-sudo sed '1i\'$'\n''oai-pc'$'\n' /etc/hostname
+echo Create file hostname...
+sudo su
+echo 'oai-pc' > /etc/hostname
+exit
