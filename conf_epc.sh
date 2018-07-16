6ccf3dcc4bd35e910bfcf0f82658ca21d2816a73
David Raditya K <32066453+davidraditya@users.noreply.github.com>
2018-07-16 11:46:11 +0800

    v7.16.18

--- conf_epc.sh
+++ conf_epc.sh
@@ -14,11 +14,11 @@ sed -i -e 's/TAC = "11";/TAC = "4";/g' epc.conf.in
 sed -i -e 's/TAC = "10";/TAC = "5";/g' epc.conf.in
 sed -i -e 's/= "127.0.0.1:5656";/= "CONSOLE";/g' epc.conf.in
 
-#Ask for previously recorded eth0 IP $newipadd
-read -p "Enter previously recorded eth0 IP: " newipadd
+#Ask for previously recorded eth0 IP $newipadd			// "read" command can't be used in POWDER
+#read -p "Enter previously recorded eth0 IP: " newipadd	// "read" command can't be used in POWDER
 
-#change PGW_IPV4_ADDRESS_FOR_SGI with new IP address
-sed -i -e 's/192.168.12.17/$newipadd/g' epc.conf.in
+#change PGW_IPV4_ADDRESS_FOR_SGI with new IP address 	// "read" command can't be used in POWDER
+#sed -i -e 's/192.168.12.17/$newipadd/g' epc.conf.in	// "read" command can't be used in POWDER
 
 sed -i -e 's/192.168.106.12/140.118.31.99/g' epc.conf.in
 sed -i -e 's/192.168.12.100/8.8.8.8/g' epc.conf.in
